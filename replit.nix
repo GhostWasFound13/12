9d70b07eebf6e3e3d92d598c636749bf41ca96f2
Ghost <87599595+ghostLuna75@users.noreply.github.com>
2023-01-24 09:28:32 +0000

    loL

--- replit.nix
+++ replit.nix
@@ -1,9 +1,8 @@
 { pkgs }: {
 	deps = [
-		pkgs.unzip
-  pkgs.nodejs-18_x
-    pkgs.nodePackages.typescript-language-server
-    pkgs.yarn
-    pkgs.replitPackages.jest
+		pkgs.nodejs-16_x
+        pkgs.nodePackages.typescript-language-server
+        pkgs.yarn
+        pkgs.replitPackages.jest
 	];
 }
